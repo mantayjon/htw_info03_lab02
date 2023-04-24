cadf6d69e0382bd0b9357660fcbbede466b0eb7d
mantayjon <Jonas.Mantay@Student.HTW-Berlin.de>
2023-04-24 10:33:59 +0200

    Update test_SortFilesByEvent.py

--- src/test_SortFilesByEvent.py
+++ src/test_SortFilesByEvent.py
@@ -1,33 +1,21 @@
 import unittest
 from sortFilesByEvent import SortFiles
-import main
 from fileObject import File
 
-jpgTest = ['DSC03767.jpeg', 'DSC03766.jpg']
-rawTest = ['DSC03965.ARW']
-
-
-class MyTestCase(unittest.TestCase):
+class TestSortFiles(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.file01 = File('file1.jpeg', 1)
+        self.file02 = File('file2.jpg', 4)
+        self.file03 = File('file3.raw', 7)
+        self.file04 = File('file4.cr2', 2)
+        self.file05 = File('file5.arw', 9)
+
+        self.files = [self.file01, self.file02, self.file03, self.file04, self.file05]
+    def test_sortByTimeStamp(self):
+        sorter = SortFiles(self.files)
+        jpeg, raw = sorter.sortByDataType()
 
 
-    '''
-    def test_folderPath(self):
-        print()
-        folderPath = ["Lab02/Pictures"]
-        self.assertTrue(folderPath == "Lab02/Pictures")
-
-
-    def test_sortByDataType(self):
-        arrJpg = SortFiles.sortByDataType(self)
-        arrRaw = SortFiles.sortByDataType(self)
-
-        self.assertTrue(arrJpg == jpgTest)
-        self.assertTrue(arrRaw == rawTest)
-'''
-
-
-if __name__ == '__main__':
+if '__name__' == '__main__':
     unittest.main()
