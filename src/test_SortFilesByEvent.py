84d91e79e7ae4cabc8428f0ac32b3f96b15bb12e
mantayjon <Jonas.Mantay@Student.HTW-Berlin.de>
2023-04-24 10:51:20 +0200

    Update test_SortFilesByEvent.py

--- src/test_SortFilesByEvent.py
+++ src/test_SortFilesByEvent.py
@@ -14,15 +14,31 @@ class TestSortFiles(unittest.TestCase):
 
         self.files = [self.file01, self.file02, self.file03, self.file04, self.file05]
 
-    def test_sortByTimeStamp(self):
+    def test_sortByFileName(self):
         sorter = SortFiles(self.files)
         jpeg, raw = sorter.sortByDataType()
 
+        expJpeg = [self.file01, self.file02]
+        expRaw = [self.file03, self.file04, self.file05]
+
         self.assertEqual(len(jpeg), 2)
-        self.assertEqual(len(raw), 3)
+        self.assertEqual(jpeg, expJpeg)
         self.assertIn(self.file01, jpeg)
         self.assertIn(self.file02, jpeg)
+
+        self.assertEqual(len(raw), 3)
+        self.assertEqual(raw, expRaw)
         self.assertIn(self.file03, raw)
+        self.assertIn(self.file04, raw)
+        self.assertIn(self.file05, raw)
+
+    def test_sortByTimeStamp(self):
+        sorter = SortFiles(self.files)
+        sorted_Timestamp = sorter.sortByTimeStamp()
+
+        expStamp = [self.file01, self.file04, self.file02, self.file03, self.file05]
+
+        self.assertEqual(sorted_Timestamp, expStamp)
 
 
 if '__name__' == '__main__':
